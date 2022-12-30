3da478df7b803b0f26c8b6a1fc77940c182e4a05
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2022-12-30 10:31:13 +0200

    Update Statistics.jsx

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,12 +5,11 @@ import PropTypes from "prop-types";
 export const  Statistics = ({good,neutral,bad,total,positiveFeedbak}) =>{
 return (
     <div>
-      <h2 className = {styles.name}>Statistics</h2>        
-        <div className= {styles.list}>
-          <p >Good: {good}</p>
-          <p >Neutral: {neutral}</p>  
-          <p >Bad: {bad}</p>             
-        </div>                  
+      <div className= {styles.list}>
+        <p >Good: {good}</p>
+        <p >Neutral: {neutral}</p>  
+        <p >Bad: {bad}</p>             
+      </div>                  
       <div className= {styles.data}>
         <p>Total: {total}</p>
         <p>Positive feedback: {positiveFeedbak} %</p>
